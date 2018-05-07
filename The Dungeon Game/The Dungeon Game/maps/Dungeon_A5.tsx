32af544da3efefdf96481edbba070b1f45fed2c1
JDBordelon <38197117+JDBordelon@users.noreply.github.com>
2018-05-06 21:14:07 -0500

    Delete Dungeon_A5.tsx

--- The Dungeon Game/The Dungeon Game/maps/Dungeon_A5.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Dungeon_A5" tilewidth="32" tileheight="32" tilecount="128" columns="8">
- <image source="Dungeon_A5.png" width="256" height="512"/>
-</tileset>
